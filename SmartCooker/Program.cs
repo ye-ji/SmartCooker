d25a579402f7a45b5e3f3f4365bfb22b1cd23477
yj.lee <yj.lee@ysmcorp.com>
2017-05-08 21:08:24 +0900

    Add project files.

---
+++ SmartCooker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCooker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
